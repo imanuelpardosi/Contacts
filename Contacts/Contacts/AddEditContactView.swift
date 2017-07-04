11d8b3c93d7dea66ffee3bac98bb62e47fe6eb48
Imanuel Pardosi <ipardosi@dyned.com>
2017-07-05 00:54:12 +0700

    add call, message, email and change favourite

--- Contacts/Contacts/AddEditContactView.swift
+++ Contacts/Contacts/AddEditContactView.swift
@@ -9,23 +9,31 @@
 import Foundation
 import UIKit
 
-class AddEditContactView: UIViewController {
+class AddEditContactView: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
     
     var presenter: AddEditContactPresenterProtocol?
     var hud: HUD = HUD()
     var uiViewUtilities: UIViewUtilities = UIViewUtilities()
     var colorUtilities: ColorUtilities = ColorUtilities()
+    let picker = UIImagePickerController()
     
+    @IBOutlet weak var popUp: UIView!
+    @IBOutlet weak var gradientView: UIView!
     @IBOutlet weak var firstName: UITextField!
     @IBOutlet weak var lastName: UITextField!
     @IBOutlet weak var mobile: UITextField!
     @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var cameraIcon: UIImageView!
     var id: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        picker.delegate = self
         presenter?.viewDidLoad()
+        popUp.isHidden = true
         
         let backItem = UIBarButtonItem()
         backItem.title = "Cancel"
@@ -37,8 +45,15 @@ class AddEditContactView: UIViewController {
         self.navigationController?.view.backgroundColor = UIColor.white
         
         self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(openCamera(tapGestureRecognizer:)))
+        cameraIcon.isUserInteractionEnabled = true
+        cameraIcon.addGestureRecognizer(tapGestureRecognizer)
+        
+        uiViewUtilities.circleView(views: profilePicture, cameraIcon)
+        uiViewUtilities.setGradientBackground(topColor: "#FFFFFF", bottomColor: "#50E3C2", uiView: gradientView)
     }
-    
+
     func doneTapped() {
         showLoading()
         if presenter?.method == "Edit" {
@@ -47,6 +62,52 @@ class AddEditContactView: UIViewController {
             presenter?.addContact(id: id, firstName: firstName.text!, lastName: lastName.text!, phoneNumber: mobile.text!, email: email.text!)
         }
         
+    }
+    
+    func openCamera(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+        popUp.isHidden = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : AnyObject])
+    {
+        var  chosenImage = UIImage()
+        chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage //2
+        profilePicture.contentMode = .scaleAspectFit //3
+        profilePicture.image = chosenImage //4
+        dismiss(animated:true, completion: nil) //5
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func openCamera(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.allowsEditing = false
+            picker.sourceType = UIImagePickerControllerSourceType.camera
+            picker.cameraCaptureMode = .photo
+            picker.modalPresentationStyle = .fullScreen
+            present(picker,animated: true,completion: nil)
+            popUp.isHidden = true
+        } else {
+            
+        }
+    }
+    
+    @IBAction func openLibrary(_ sender: Any) {
+        picker.allowsEditing = false
+        picker.sourceType = .photoLibrary
+        picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        picker.modalPresentationStyle = .popover
+        present(picker, animated: true, completion: nil)
+        //picker.popoverPresentationController?.barButtonItem = sender
+        popUp.isHidden = true
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        popUp.isHidden = true
     }
 }
 
@@ -58,6 +119,12 @@ extension AddEditContactView: AddEditContactViewProtocol {
         lastName.text = contact.lastName
         mobile.text = contact.phoneNumber
         email.text = contact.email
+        
+        let placeholderImage = UIImage(named: "user")!
+        let profilePictureUrl = URL(string: (contact.profilePicture))
+        profilePicture?.af_setImage(withURL: profilePictureUrl!, placeholderImage: placeholderImage)
+        
+        uiViewUtilities.borderView(views: profilePicture, borderWidth: 3, color: UIColor.white)
     }
     
     func showError(errorMessage: String) {
